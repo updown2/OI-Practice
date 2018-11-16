1e56bb72f3a16fba8da7e3f966ccc71e9d54a138
updown2 <riya.aro@gmail.com>
2018-11-16 12:32:03 -0800

    Update Beads.cpp

--- APOI/2008/Beads.cpp
+++ APOI/2008/Beads.cpp
@@ -2,7 +2,7 @@
 use persistence for the location of the beads and the swapper number for the time
 */
 #include <bits/stdc++.h>
-//#include "lib1739.h"
+#include "lib1739.h"
 using namespace std;
 typedef long long ll;
 #define For(i, a, b) for(int i=a; i<b; i++)
@@ -24,12 +24,12 @@ vector<int> loc[MAXN], tim[MAXN];
 
 int main() {
     //ifstream cin("test.in");
-    //ofstream cout("test.out");
     ios_base::sync_with_stdio(0); cin.tie(0);
-    cin >> N >> M;
+    //cin >> N >> M;
+    scanf("%d%d", &N, &M);
     ffi {bead[i] = i; loc[i].pb(i); tim[i].pb(0);}
     ffj {
-        int a; cin >> a;
+        int a; scanf("%d", &a);//cin >> a;
         int b1 = bead[a], b2 = bead[a+1];
         bead[a] = b2, bead[a+1] = b1;
         loc[b1].pb(a+1), loc[b2].pb(a);
@@ -37,12 +37,12 @@ int main() {
     }
     //ffi {w<< i s ":" <<e;For (j, 0, loc[i].size()) w<< loc[i][j] s "at" s tim[i][j]<<e;}
 
-    //Q = getNumQuestions();
-    cin >> Q;
+    Q = getNumQuestions();
+    //cin >> Q;
     For (q, 0, Q) {
         int K, J;
-        //getQuestion(K, J);
-        cin >> K >> J;
+        getQuestion(K, J);
+        //cin >> K >> J;
         int a = 0, b = tim[K].size()-1;
         /// find the last tim less than or equal to J
         while (a != b) {
@@ -50,7 +50,7 @@ int main() {
             if (tim[K][mid] <= J) a = mid;
             else b = mid-1;
         }
-        //answer(loc[K][a]);
-        w<< loc[K][a]<<e;
+        answer(loc[K][a]);
+        //w<< loc[K][a]<<e;
     }
 }
